b4d5202afb5c4db446bc4d8d4de9dfd5fef8d981
trinyo <zolika200510@gmail.com>
2025-11-08 11:20:18 +0100

    created header functions for handling data

---
+++ include/fajlkezeles.h
@@ -0,0 +1,14 @@
+/**
+ * @brief Reads the files from the data directory.
+ * If reading fails it calls createNonExistantFiles.
+ *
+ */
+void readFromFiles();
+/**
+ * @brief Creates the files.
+ */
+void createNonExistantFiles();
+/**
+ * @brief Saves the list in to text format in the data directory.
+ */
+void writeToFiles();
