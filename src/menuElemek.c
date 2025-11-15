4be63ede80dbd5aaeb970474f6b782687334a954
trinyo <zolika200510@gmail.com>
2025-11-15 08:50:43 +0100

    implemented wait for enter

---
+++ src/menuElemek.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+void displayMainMenu() {
+  printf("==========================================\n");
+  printf("|       HALLGATÓI NYILVÁNTARTÁS          |\n");
+  printf("==========================================\n\n");
+
+  printf("Válasszon menüpontot a számozott opciók közül:\n\n");
+
+  printf("[1]  Listázás (Adatok szűrése, rangsorolása)\n");
+  printf("[2]  Új adat felvétele (Hallgató/Oktató rögzítése)\n");
+  printf("[3]  Információ szerkesztése / törlése\n");
+  printf("------------------------------------------\n");
+  printf("[0]  Kilépés a programból\n\n");
+
+  printf("Adja meg a választott számot: ");
+}
+
+void displayListMenu() {
+  printf("\n==========================================\n");
+  printf("|            LISTÁZÁS MENÜ               |\n");
+  printf("==========================================\n");
+
+  printf("\nVálasszon egy listázási / elemzési opciót:\n");
+
+  printf("\n--- ALAP LISTÁK ---\n");
+  printf("[11] Összes hallgatói rekord listázása\n");
+  printf("[12] Összes oktatói rekord listázása\n");
+  printf("[13] Hallgatók listázása csoport szerint (pl. G01)\n");
+
+  printf("\n--- SZŰRÉS ÉS PÓTLÁS ---\n");
+  printf("[21] PZH-ra kötelezett hallgatók listája (Pót-ZH)\n");
+  printf("[22] Pótlásra kötelezett hallgatók listája (Nagy házi, labor)\n");
+
+  printf("\n--- RANGSOROLÁS ÉS STATISZTIKA ---\n");
+  printf(
+      "[31] Hallgatói rangsor (Összpontszám szerint, csökkenő sorrendben)\n");
+  printf("[32] Legtöbbet hiányzó hallgatók listája\n");
+  printf("[33] Csoportok összehasonlítása (Átlagpontszám szerint)\n");
+
+  printf("------------------------------------------\n");
+  printf("[9]  Vissza a főmenübe\n\n");
+
+  printf("Adja meg a választott számot: ");
+}
+
+void displayNewDataMenu() {
+  printf("\n==========================================\n");
+  printf("|       ÚJ ADAT FELVÉTELE MENÜ           |\n");
+  printf("==========================================\n\n");
+
+  printf("Milyen típusú új rekordot szeretne felvenni?\n\n");
+
+  printf("[1]  Új HALLGATÓ felvétele\n");
+  printf("[2]  Új OKTATÓ felvétele\n");
+  printf("------------------------------------------\n");
+  printf("[9]  Vissza a főmenübe\n\n");
+
+  printf("Adja meg a választott számot: ");
+}
+
+void clear_screen() {
+#ifdef _WIN32
+  system("cls");
+#else
+  system("clear");
+#endif
+}
+void waitForEnter() {
+  printf("\nNyomd meg az ENTER billentyűt a továbblépéshez...");
+
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF) {
+  }
+
+  getchar();
+}
