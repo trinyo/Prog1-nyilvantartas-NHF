e36893c4c23faecbe67aeae55b164962fff8a120
trinyo <zolika200510@gmail.com>
2025-11-15 08:51:36 +0100

    implemented list menu and added ini list creation

--- src/main.c
+++ src/main.c
@@ -1,96 +1,42 @@
+// global libraries
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-void displayMainMenu() {
-  printf("==========================================\n");
-  printf("|       HALLGATÓI NYILVÁNTARTÁS          |\n");
-  printf("==========================================\n\n");
-
-  printf("Válasszon menüpontot a számozott opciók közül:\n\n");
-
-  printf("[1]  Listázás (Adatok szűrése, rangsorolása)\n");
-  printf("[2]  Új adat felvétele (Hallgató/Oktató rögzítése)\n");
-  printf("[3]  Információ szerkesztése / törlése\n");
-  printf("------------------------------------------\n");
-  printf("[0]  Kilépés a programból\n\n");
-
-  printf("Adja meg a választott számot: ");
-}
-
-void displayListMenu() {
-  printf("\n==========================================\n");
-  printf("|            LISTÁZÁS MENÜ               |\n");
-  printf("==========================================\n");
-
-  printf("\nVálasszon egy listázási / elemzési opciót:\n");
-
-  printf("\n--- ALAP LISTÁK ---\n");
-  printf("[11] Összes hallgatói rekord listázása\n");
-  printf("[12] Összes oktatói rekord listázása\n");
-  printf("[13] Hallgatók listázása csoport szerint (pl. G01)\n");
-
-  printf("\n--- SZŰRÉS ÉS PÓTLÁS ---\n");
-  printf("[21] PZH-ra kötelezett hallgatók listája (Pót-ZH)\n");
-  printf("[22] Pótlásra kötelezett hallgatók listája (Nagy házi, labor)\n");
-
-  printf("\n--- RANGSOROLÁS ÉS STATISZTIKA ---\n");
-  printf(
-      "[31] Hallgatói rangsor (Összpontszám szerint, csökkenő sorrendben)\n");
-  printf("[32] Legtöbbet hiányzó hallgatók listája\n");
-  printf("[33] Csoportok összehasonlítása (Átlagpontszám szerint)\n");
-
-  printf("------------------------------------------\n");
-  printf("[9]  Vissza a főmenübe\n\n");
-
-  printf("Adja meg a választott számot: ");
-}
-
-void displayNewDataMenu() {
-  printf("\n==========================================\n");
-  printf("|       ÚJ ADAT FELVÉTELE MENÜ           |\n");
-  printf("==========================================\n\n");
-
-  printf("Milyen típusú új rekordot szeretne felvenni?\n\n");
-
-  printf("[1]  Új HALLGATÓ felvétele\n");
-  printf("[2]  Új OKTATÓ felvétele\n");
-  printf("------------------------------------------\n");
-  printf("[9]  Vissza a főmenübe\n\n");
-
-  printf("Adja meg a választott számot: ");
-}
-
-void clear_screen() {
-#ifdef _WIN32
-  system("cls");
-#else
-  system("clear");
-#endif
-}
+// local headers
+#include "../include/adatkezeles.h"
+#include "../include/fajlkezeles.h"
+#include "../include/menuElemek.h"
+#include "../include/strukturak.h"
 
 int main(int argc, char *argv[]) {
   int userInput;
-  clear_screen();
-  displayMainMenu();
+  Student *studentsList = readStudentFileAndCreateStudentList();
+  Teacher *teacherList = readTeacherFileAndCreateTeacherList();
   do {
+    clear_screen();
+    displayMainMenu();
     scanf("%i", &userInput);
     switch (userInput) {
+      // list menu
     case 1:
-      clear_screen();
-      displayListMenu();
+      mainLoopForDataHandling(studentsList, teacherList);
       break;
+      // add new data
     case 2:
       clear_screen();
       displayNewDataMenu();
       break;
+      // update data or remove
     case 3:
       clear_screen();
       break;
-    case 9:
-      displayMainMenu();
+    default:
+      printf("Nem volt megfelelő a bemenet! \n");
+      waitForEnter();
     }
   } while (userInput != 0);
 
+  freeStudentsLinkedList(studentsList);
+  freeTeachersLinkedList(teacherList);
   return EXIT_SUCCESS;
 }
